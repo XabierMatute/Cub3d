ee89bf39754d9ede95449b7727db8ba8c1e7af31
Xabier Matute Rusinol <xmatute-@c3r6s1.42urduliz.com>
2023-04-02 19:30:31 +0200

    commit general

--- mandatory/libft/ft_lstnew_bonus.c
+++ mandatory/libft/ft_lstnew_bonus.c
@@ -1,28 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 19:50:07 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/09/22 12:08:21 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/31 12:34:43 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// t_list	*ft_lstnew(void *content)
-// {
-// 	t_list	*node;
-
-// 	node = (t_list *)malloc(sizeof(t_list));
-// 	if (!node)
-// 		return (NULL);
-// 	node->content = content;
-// 	node->next = NULL;
-// 	return (node);
-// }
 
 t_list	*ft_lstnew(void *content)
 {
