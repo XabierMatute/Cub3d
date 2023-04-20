a743d1959066fcade236913c527ddcdbf09814e2
Xabier Matute Rusinol <xmatute-@c3r6s1.42urduliz.com>
2023-04-20 22:36:46 +0200

    errors/color_r_error.c mejorado

--- mandatory/errors/color_r_error.c
+++ mandatory/errors/color_r_error.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 17:33:47 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/04/14 19:09:55 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/04/20 21:27:38 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 int	color_r_error(char *id)
 {
-	printf("❌Error: en %s hay caracteres fuera de rango\n", id);
+	printf("❌Error: en %s hay valores fuera de rango\n", id);
 	return (0);
 }
