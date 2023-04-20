87cf5733dd7af1010f219197456d3ceab45638a0
Jon Perez <jperez@c3r5s4.42urduliz.com>
2023-04-20 20:26:08 +0200

    n ft_raycasting_aux REOGANIZED FUNCTINS

--- mandatory/raycasting/ft_raycasting_aux.c
+++ mandatory/raycasting/ft_raycasting_aux.c
@@ -6,101 +6,55 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 19:04:14 by jperez            #+#    #+#             */
-/*   Updated: 2023/04/20 20:13:00 by jperez           ###   ########.fr       */
+/*   Updated: 2023/04/20 20:25:19 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../cub3d.h"
+#include "../cub3d.h"
 
-double	ft_get_distance(double player_x, double player_y, double ray_x, double ray_y)
+double	ft_get_distance(double player_x, double player_y, \
+	double ray_x, double ray_y)
 {
 	return (sqrt(pow(ray_x - player_x, 2) + pow(ray_y - player_y, 2)));
 }
 
 //El error esta en que el map lo has puesto en las funciones como int
-int	ft_check_wall(char **map, double ray_x, double ray_y)
+int	ft_check_wall(char **map, int x, int y)
 {
-	int	x;
-	int	y;
-	
-	x = (int)floor(ray_x / UNIT);
-	y = (int)floor(ray_y / UNIT);
-
-	#ifdef DEBUG
-	printf("Check_wall\n");
-	printf("X: %d\n", x);
-	printf("Y: %d\n", y);
-	#endif
-
-	if (!((0 <= x && x < (int)ft_strlen(map[0])) && (0 <= y && y < ft_args_len(map))))
+	if (!((0 <= x && x < (int)ft_strlen(map[0])) && \
+		(0 <= y && y < ft_args_len(map))))
 		return (1);
 	if ('0' < map[y][x] && map[y][x] <= '9')
 		return (1);
 	return (0);
 }
 
-int	ft_check_vertical_walls(char **map, double ray_x, double ray_y, double angle)
+int	ft_check_vertical_walls(char **map, double ray_x, double ray_y, \
+	double angle)
 {
 	int	x;
 	int	y;
-	
+
 	x = (int)floor(ray_x / UNIT);
 	y = (int)floor(ray_y / UNIT);
-
-	#ifdef DEBUG
-	printf("Check_wall\n");
-	printf("X: %d\n", x);
-	printf("Y: %d\n", y);
-	#endif
-
-
 	if (!ft_angle_in_range(M_PI_2, M_PI_3_2, angle))
-	{
-		if (!((0 <= x && x < (int)ft_strlen(map[0])) && (0 <= y && y < ft_args_len(map))))
-			return (1);
-		if ('0' < map[y][x] && map[y][x] <= '9')
-			return (1);
-	}
+		return (ft_check_wall(map, x, y));
 	else
-	{
-		x--;
-		if (!((0 <= x && x < (int)ft_strlen(map[0])) && (0 <= y && y < ft_args_len(map))))
-			return (1);
-		if ('0' < map[y][x] && map[y][x] <= '9')
-			return (1);
-	}
+		return (ft_check_wall(map, --x, y));
 	return (0);
 }
 
-int	ft_check_horizontal_walls(char **map, double ray_x, double ray_y, double angle)
+int	ft_check_horizontal_walls(char **map, double ray_x, double ray_y, \
+	double angle)
 {
 	int	x;
 	int	y;
-	
+
 	x = (int)floor(ray_x / UNIT);
 	y = (int)floor(ray_y / UNIT);
-
-	#ifdef DEBUG
-	printf("Check_wall\n");
-	printf("X: %d\n", x);
-	printf("Y: %d\n", y);
-	#endif
-
-
 	if (!ft_angle_in_range(0, M_PI, angle))
-	{
-		if (!((0 <= x && x < (int)ft_strlen(map[0])) && (0 <= y && y < ft_args_len(map))))
-			return (1);
-		if ('0' < map[y][x] && map[y][x] <= '9')
-			return (1);
-	}
+		return (ft_check_wall(map, x, y));
 	else
-	{
-		y--;
-		if (!((0 <= x && x < (int)ft_strlen(map[0])) && (0 <= y && y < ft_args_len(map))))
-			return (1);
-		if ('0' < map[y][x] && map[y][x] <= '9')
-			return (1);
-	}
+		return (ft_check_wall(map, x, --y));
 	return (0);
 }
