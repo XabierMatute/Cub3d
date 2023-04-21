4fde46b18990295ed8dcb418433182bb67a4acd3
Jon Perez <jperez@c3r2s2.42urduliz.com>
2023-04-21 18:12:15 +0200

    protect put and get pixel

--- mandatory/utils/ft_manage_imgs.c
+++ mandatory/utils/ft_manage_imgs.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 18:41:56 by jperez            #+#    #+#             */
-/*   Updated: 2023/04/20 20:20:40 by jperez           ###   ########.fr       */
+/*   Updated: 2023/04/21 18:08:55 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
+	if (x >= WIN_WIDTH)
+		x = WIN_WIDTH - 1;
+	else if (x < 0)
+		x = 0;
+	if (y >= WIN_HEIGHT)
+		y = WIN_HEIGHT - 1;
+	else if (y < 0)
+		y = 0;
 	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
 	*(unsigned int*)dst = color;
 }
@@ -28,8 +36,12 @@ int ft_get_texture_pixel(t_img *img, int x, int y)
 
 	if (x >= img->width)
 		x = img->width - 1;
+	else if (x < 0)
+		x = 0;
 	if (y >= img->height)
 		y = img->height - 1;
+	else if (y < 0)
+		y = 0;
 	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
 	return (*(unsigned int*)dst);
 }
