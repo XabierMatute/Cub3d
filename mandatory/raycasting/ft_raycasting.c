cd92b51950fec29e7a5efe49378f394874349598
Jon Perez <jperez@c3r2s2.42urduliz.com>
2023-04-21 18:16:48 +0200

    normi ft_raycasting.c

--- mandatory/raycasting/ft_raycasting.c
+++ mandatory/raycasting/ft_raycasting.c
@@ -6,11 +6,11 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 19:53:53 by jperez            #+#    #+#             */
-/*   Updated: 2023/04/20 19:36:12 by jperez           ###   ########.fr       */
+/*   Updated: 2023/04/21 18:15:52 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../cub3d.h"
+#include "../cub3d.h"
 
 double	ft_return_min(double horizontal_colision, double vertical_colision)
 {
@@ -20,7 +20,8 @@ double	ft_return_min(double horizontal_colision, double vertical_colision)
 		return (horizontal_colision);
 }
 
-double	ft_return_orientation(double horizontal_colision, double vertical_colision)
+double	ft_return_orientation(double horizontal_colision, \
+	double vertical_colision)
 {
 	if (vertical_colision < horizontal_colision)
 		return (VERTICAL);
@@ -28,31 +29,19 @@ double	ft_return_orientation(double horizontal_colision, double vertical_colisio
 		return (HORIZONTAL);
 }
 
-t_colision *ft_raycasting(double player_x, double player_y, double angle, char **map)
+t_colision	*ft_raycasting(double player_x, double player_y, double angle, \
+	char **map)
 {
 	t_colision	*colision;
-	double	horizontal_colision;
-	double	vertical_colision;
-
+	double		horizontal_colision;
+	double		vertical_colision;
 
 	colision = (t_colision *)malloc(sizeof(t_colision));
-	#ifdef DEBUG
-	printf("-------Horizontal-Colisions-------\n");
-	#endif
-	horizontal_colision = ft_horizontal_colisions(player_x, player_y, angle, map);
-
-	#ifdef DEBUG
-	printf("\n-------Vertical-Colisions-------\n");
-	#endif
+	horizontal_colision = ft_horizontal_colisions(player_x, player_y, angle, \
+		map);
 	vertical_colision = ft_vertical_colisions(player_x, player_y, angle, map);
-
-	#ifdef DEBUG
-	printf("\n");
-	printf("Horizontal_colision: %f\n", horizontal_colision);
-	printf("Vertical_colision: %f\n", vertical_colision);
-	#endif
-
 	colision->distance = ft_return_min(horizontal_colision, vertical_colision);
-	colision->orientation = ft_return_orientation(horizontal_colision, vertical_colision);
+	colision->orientation = ft_return_orientation(horizontal_colision, \
+		vertical_colision);
 	return (colision);
 }
