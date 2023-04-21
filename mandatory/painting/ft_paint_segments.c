f89a9f4ff1386fe148ac8ca630265211846a88ee
Jon Perez <jperez@c3r2s2.42urduliz.com>
2023-04-21 19:56:34 +0200

    ft_paint_segments.c

--- mandatory/painting/ft_paint_segments.c
+++ mandatory/painting/ft_paint_segments.c
@@ -6,24 +6,26 @@
 /*   By: jperez <jperez@student.42urduliz.>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 19:49:48 by jperez            #+#    #+#             */
-/*   Updated: 2023/04/20 20:12:52 by jperez           ###   ########.fr       */
+/*   Updated: 2023/04/21 19:55:46 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../cub3d.h"
+#include "../cub3d.h"
 
-void	ft_paint_ceiling(t_img *img, int img_x, double wall_height, int sky_color)
+void	ft_paint_ceiling(t_img *img, int img_x, double wall_height, \
+	int sky_color)
 {
 	int	img_y;
 	int	end;
 
-	end	= (WIN_HEIGHT - wall_height) / 2;
+	end = (WIN_HEIGHT - wall_height) / 2;
 	img_y = -1;
 	while (++img_y < end)
 		my_mlx_pixel_put(img, img_x, img_y, sky_color);
 }
 
-void	ft_paint_floor(t_img *img, int img_x, double wall_height, int floor_color)
+void	ft_paint_floor(t_img *img, int img_x, double wall_height, \
+	int floor_color)
 {
 	int	img_y;
 
@@ -46,9 +48,11 @@ int	ft_get_wall_start(int wall_height, int *texture_gap)
 	}
 }
 
-# define IMG		0
-# define TEXTURE	1
-void	ft_paint_wall(t_img *img, t_img *texture, int indexes[], double wall_height)
+#define IMG		0
+#define TEXTURE	1
+
+void	ft_paint_wall(t_img *img, t_img *texture, int indexes[], \
+	double wall_height)
 {
 	int		img_y;
 	int		start;
@@ -63,7 +67,8 @@ void	ft_paint_wall(t_img *img, t_img *texture, int indexes[], double wall_height
 	while (img_y < start + wall_height && img_y < WIN_HEIGHT)
 	{
 		my_mlx_pixel_put(img, indexes[IMG], img_y, \
-			ft_get_texture_pixel(texture, indexes[TEXTURE], (img_y - start + texture_gap) * scale));
+			ft_get_texture_pixel(texture, indexes[TEXTURE], \
+				(img_y - start + texture_gap) * scale));
 		img_y++;
 	}
 }
