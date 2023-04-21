b4c5529906f14dc193ff42715040190d96ab42b4
Jon Perez <jperez@c3r2s2.42urduliz.com>
2023-04-21 18:32:40 +0200

    normi ft_get_texture.c

--- mandatory/painting/ft_get_texture_y.c
+++ mandatory/painting/ft_get_texture_y.c
@@ -6,11 +6,11 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 17:49:21 by jperez            #+#    #+#             */
-/*   Updated: 2023/04/20 18:02:45 by jperez           ###   ########.fr       */
+/*   Updated: 2023/04/21 18:31:45 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../cub3d.h"
+#include "../cub3d.h"
 
 static double	ft_get_ray_x(double player_x, double angle)
 {
@@ -20,12 +20,15 @@ static double	ft_get_ray_x(double player_x, double angle)
 		return (floor(player_x / UNIT) * UNIT + UNIT);
 }
 
-static double	ft_get_ray_y(double player_y, double player_x, double ray_x, double angle)
+static double	ft_get_ray_y(double player_y, double player_x, double ray_x, \
+	double angle)
 {
 	if (ft_angle_in_range(0, M_PI, angle))
-		return (player_y - fabs(player_x - ray_x) * tan(ft_normalize_angle(angle)));
+		return (\
+		player_y - fabs(player_x - ray_x) * tan(ft_normalize_angle(angle)));
 	else
-		return (player_y + fabs(player_x - ray_x) * tan(ft_normalize_angle(angle)));
+		return (\
+		player_y + fabs(player_x - ray_x) * tan(ft_normalize_angle(angle)));
 }
 
 static void	ft_find_gap(double *ray_gap_x, double *ray_gap_y, double angle)
@@ -34,32 +37,27 @@ static void	ft_find_gap(double *ray_gap_x, double *ray_gap_y, double angle)
 		*ray_gap_x = 0 - UNIT;
 	else
 		*ray_gap_x = UNIT;
-	
 	if (ft_angle_in_range(0, M_PI, angle))
 		*ray_gap_y = 0 - fabs(*ray_gap_x) * tan(ft_normalize_angle(angle));
 	else
 		*ray_gap_y = fabs(*ray_gap_x) * tan(ft_normalize_angle(angle));
 }
 
-int	ft_get_texture_y(double player_x, double player_y,  double angle, char **map)
+int	ft_get_texture_y(double player_x, double player_y, double angle, \
+	char **map)
 {
 	double	ray_x;
 	double	ray_y;
 	double	ray_gap_x;
 	double	ray_gap_y;
 
-
-
 	ray_x = ft_get_ray_x(player_x, angle);
 	ray_y = ft_get_ray_y(player_y, player_x, ray_x, angle);
-
 	ft_find_gap(&ray_gap_x, &ray_gap_y, angle);
-
 	while (!ft_check_vertical_walls(map, ray_x, ray_y, angle))
 	{
 		ray_x += ray_gap_x;
 		ray_y += ray_gap_y;
 	}
-
 	return ((int)floor(ray_y) % UNIT);
 }
