b5b1d800221b38c270ccbbbcd625fa9850985e20
Jon Perez <jperez@c3r2s2.42urduliz.com>
2023-04-21 19:49:42 +0200

    normi lighting_gun.c

--- mandatory/painting/ft_lightning_gun.c
+++ mandatory/painting/ft_lightning_gun.c
@@ -6,11 +6,11 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 13:47:24 by jperez            #+#    #+#             */
-/*   Updated: 2023/04/19 17:53:50 by jperez           ###   ########.fr       */
+/*   Updated: 2023/04/21 18:33:34 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../cub3d.h"
+#include "../cub3d.h"
 
 void	*ft_send_height_and_angle(double wall_height, double angle)
 {
@@ -36,44 +36,13 @@ void	ft_lightning_gun(char **map, double angle, t_game *game)
 	ray_x = 0;
 	mlx_clear_window(game->mlx, game->window);
 	img = ft_create_img(game->mlx, WIN_WIDTH, WIN_HEIGHT);
-
-	#ifdef DEBUG
-	printf("min: %f\nmax: %f\n", min_angle, max_angle);
-	#endif
-
 	while (ray_x < WIN_WIDTH)
 	{
-		#ifdef DEBUG
-		printf("==============================================\n");
-		printf("		RAY: %d					 \n", ray_x);
-		printf("==============================================\n");
-		printf("Angle: %f\n", max_angle);
-		printf("Normalize-angle: %f\n", ft_normalize_angle(max_angle));
-		printf("Player_x: %f\n", game->player[x]);
-		printf("Player_y: %f\n\n", game->player[y]);
-		#endif
-
-
-		/*
-		wall_height = ft_calculate_wall_height(ft_raycasting(\
-			mlx->player_x, mlx->player_y, max_angle, mlx->map));
-		printf("Wall_height: %f\n", wall_height);
-		*/
-
 		ft_paint_column(game, img, ray_x, max_angle);
-
-		//ft_edit_img(img, mlx, x, wall_height);
-
-
 		max_angle -= ANGLE_GAP;
 		if (max_angle < 0)
 			max_angle += 2 * M_PI;
-
-
 		ray_x++;
-		#ifdef DEBUG
-		printf("\n\n\n\n");
-		#endif
 	}
 	mlx_put_image_to_window(game->mlx, game->window, img->mlx_img, 0, 0);
 	free(img);
