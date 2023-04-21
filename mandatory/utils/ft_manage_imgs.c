da873ee1960b11578a00758645626c183f8f9cd4
Jon Perez <jperez@c3r2s2.42urduliz.com>
2023-04-21 20:00:50 +0200

    normi ft_manage_imgs.c

--- mandatory/utils/ft_manage_imgs.c
+++ mandatory/utils/ft_manage_imgs.c
@@ -6,13 +6,11 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 18:41:56 by jperez            #+#    #+#             */
-/*   Updated: 2023/04/21 18:08:55 by jperez           ###   ########.fr       */
+/*   Updated: 2023/04/21 19:59:49 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../cub3d.h"
-
-# define WIDTH 2
+#include "../cub3d.h"
 
 void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
@@ -27,10 +25,10 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 	else if (y < 0)
 		y = 0;
 	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
-int ft_get_texture_pixel(t_img *img, int x, int y)
+int	ft_get_texture_pixel(t_img *img, int x, int y)
 {
 	char	*dst;
 
@@ -43,16 +41,17 @@ int ft_get_texture_pixel(t_img *img, int x, int y)
 	else if (y < 0)
 		y = 0;
 	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
-	return (*(unsigned int*)dst);
+	return (*(unsigned int *)dst);
 }
 
 t_img	*ft_create_img(void *mlx, int width, int height)
 {
-	t_img *img;
+	t_img	*img;
 
 	img = (t_img *)malloc(sizeof(t_img));
 	img->mlx_img = mlx_new_image(mlx, width, height);
-	img->addr = mlx_get_data_addr(img->mlx_img, &img->bpp, &img->line_len, &img->endian);
+	img->addr = mlx_get_data_addr(img->mlx_img, &img->bpp, &img->line_len, \
+		&img->endian);
 	return (img);
 }
 
@@ -60,10 +59,10 @@ t_img	*ft_save_xpm(t_game *game, char *path)
 {
 	t_img	*img;
 
-
 	img = (t_img *)malloc(sizeof(t_img));
-	img->mlx_img = mlx_xpm_file_to_image(game->mlx, path, &img->width, &img->height);
-	img->addr = mlx_get_data_addr(img->mlx_img, &img->bpp, &img->line_len, &img->endian);
-	return(img);
+	img->mlx_img = mlx_xpm_file_to_image(game->mlx, path, &img->width, \
+		&img->height);
+	img->addr = mlx_get_data_addr(img->mlx_img, &img->bpp, &img->line_len, \
+		&img->endian);
+	return (img);
 }
-
